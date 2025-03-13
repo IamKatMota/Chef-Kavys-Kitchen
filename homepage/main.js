6eef9a14501587775161c8c60cd3cef9be57f9be
Kat Mota <katmmota@gmail.com>
2025-03-12 19:54:00 -0500

    add form submission javascript

--- homepage/main.js
+++ homepage/main.js
@@ -3,10 +3,7 @@ const hamburger = document.getElementById('hamburger')
 const sidebar = document.getElementById('sidebar')
 const overlay = document.getElementById('overlay')
 
-//keeps track of whether menu is open, using let bc its value can change from false to true
 let menuOpen =  false
-
-//tells our doc what to do when we open and close the menu
 
 function openMenu(){
     menuOpen = true;
@@ -19,16 +16,12 @@ function closeMenu(){
     sidebar.style.width = '0px'
 }
 
-//to make it run when you click the hamburger menu
-
 hamburger.addEventListener('click', function(){
-    //tests if menu open is false 
     if (!menuOpen) {
         openMenu()
     }
 })
 
- //tests if menu open is true, if it is and you click on the overlay it will close the menu
 overlay.addEventListener('click', function(){ 
     if (menuOpen) {
         closeMenu()
@@ -38,24 +31,58 @@ overlay.addEventListener('click', function(){
 //SLIDESHOW
 let imgs = document.querySelectorAll('.slideshow img');
 let currentImg = 0; //index of first img
-const interval = 3000; //duration of the slide
+const interval = 3000; 
 
-//sets images' opacity to 0 and removes activ e from the dots' class list to reset the active slide
 let timer = setInterval(changeSlide, interval);
 
 function changeSlide(n) {
     for (var i = 0; i < imgs.length; i++) {
-      imgs[i].style.opacity = 0;
-      
+    imgs[i].style.opacity = 0;
     }
 
     currentImg = (currentImg + 1) % imgs.length;
 
     if (n != undefined) {
-      clearInterval(timer);
-      timer = setInterval(changeSlide, interval);
-      currentImg = n;
+        clearInterval(timer);
+        timer = setInterval(changeSlide, interval);
+        currentImg = n;
     }
 
     imgs[currentImg].style.opacity = 1;
-  }
+}
+
+//form submission
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    try {
+        const formData = new FormData(form);
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+        result.innerHTML = "Please wait..."
+
+        let response = await fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        let data = await response.json();
+
+        if (response.ok){
+            result.innerHTML = "Form submitted successfully";
+        }else {
+            result.innerHTML = data.message;
+        }
+    } catch (error) {
+        console.error(error);
+        result.innerHTML = "Something went wrong!";
+    }finally {
+        form.reset();
+        setTimeout(() => { result.style.display = "none"; }, 3000);
+    }
+});
